8724e6da6a8f1939cab6b83a7e1d79e2e2d950b3
aidend <aidendawes@gmail.com>
2020-08-06 21:24:36 +0200

    Test Mac
    asdsa

--- lib/testing.dart
+++ lib/testing.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+/////
 final _firestore = Firestore.instance;
 FirebaseUser loggedInUser;
 void main() async {
